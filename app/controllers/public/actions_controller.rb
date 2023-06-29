603e6d4d3410ee4df6c2aa86f8e7150cff88d365
kurosiroke <rikityanlovelove@gmail.com>
2023-06-29 12:12:04 +0000

    ヘッダーに遷移先作成

--- app/controllers/public/actions_controller.rb
+++ app/controllers/public/actions_controller.rb
@@ -1,6 +1,11 @@
 class Public::ActionsController < ApplicationController
     
-    has_many :favorites
-    has_many :comments
-    belongs_to :users
+    # has_many :favorites
+    # has_many :comments
+    # belongs_to :users
+
+
+    def show
+        @user = current_user
+    end 
 end
--- app/models/action.rb
+++ app/models/action.rb
@@ -1,6 +1,10 @@
 class Action < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # devise :database_authenticatable, :registerable,
+  #       :recoverable, :rememberable, :validatable
+         
+  has_many :favorites
+  has_many :comments
+  belongs_to :users
 end
