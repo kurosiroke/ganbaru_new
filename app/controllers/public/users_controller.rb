030bdf0d3a9ba4d211752e5ddff91e9e6d29206d
kurosiroke <rikityanlovelove@gmail.com>
2023-06-30 12:33:06 +0000

    update

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,4 +1,11 @@
 class Public::UsersController < ApplicationController
+    
+    before_action :correct_user, only: [:edit, :update]
+    
+    def index
+        @users = User.all
+    end
+    
     def show
         @user = current_user
     end 
