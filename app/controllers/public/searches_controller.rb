ce34d5e2e4b1320fd1aa165f71888d28e3d14652
kurosiroke <rikityanlovelove@gmail.com>
2023-07-25 13:00:26 +0000

    update

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,7 +1,7 @@
 class Public::SearchesController < ApplicationController
     def search
+    # userとattemptの切り替え
     @range = params[:range]
-
     if @range == "User"
       @users = User.looks(params[:search], params[:word])
     else
