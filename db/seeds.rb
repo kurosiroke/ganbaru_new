6720566320bdacf2d96a35f4483dc93121ecb3c1
kurosiroke <rikityanlovelove@gmail.com>
2023-08-10 11:38:16 +0000

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,14 @@ User.create!(
 
 Attempt.create!(
   [
-    {user_id:1, content: "テストを頑張る！", is_published_flag:true, part:0} 
+    {user_id:1, content: "テストを頑張る！", is_published_flag:true, part:0},
+    {user_id:1, content: "テストを頑張った！", is_published_flag:true, part:1},
+    {user_id:2, content: "掃除を頑張る！", is_published_flag:true, part:0},
+    {user_id:2, content: "掃除を頑張った！", is_published_flag:true, part:1}, 
+    {user_id:3, content: "勉強を頑張る！", is_published_flag:true, part:0},
+    {user_id:3, content: "勉強を頑張った！", is_published_flag:true, part:1},
+    {user_id:4, content: "本を一冊読めるよう頑張る！", is_published_flag:true, part:0},
+    {user_id:4, content: "読書できるよう時間調整を頑張った！", is_published_flag:true, part:1} 
   ]
 )
 
@@ -29,14 +36,18 @@ Tag.create!(
    {tag_type: "test1"},
    {tag_type: "test2"},
    {tag_type: "test3"},
-   {tag_type: "test4"},
-   {tag_type: "test5"},
-   {tag_type: "test6"}
   ]
 )
 
 AttemptAndTag.create!(
   [
-    {attempt_id: 1, tag_id: 1}
+    {attempt_id: 1, tag_id: 1},
+    {attempt_id: 1, tag_id: 1},
+    {attempt_id: 2, tag_id: 2},
+    {attempt_id: 2, tag_id: 2},
+    {attempt_id: 3, tag_id: 3},
+    {attempt_id: 3, tag_id: 3},
+    {attempt_id: 4, tag_id: 1},
+    {attempt_id: 4, tag_id: 1}
   ]
 )
