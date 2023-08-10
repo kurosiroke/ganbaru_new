2c179c2735d1bc523dfecdb89f5af8bfbc53df24
kurosiroke <rikityanlovelove@gmail.com>
2023-08-10 10:43:06 +0000

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,31 @@ Admin.create!(
 
 User.create!(
   [
-    {email: "t@t", name: "鯛", profile: "鯛焼きです", password: "123123"},
+    {email: "t@t", name: "鯛", profile: "鯛焼きです。", password: "123123"},
     {email: "a@a", name: "鮎", profile: "魚で占ったから鮎", password: "123123"},
-    {email: "m@m", name: "鮪", profile: "魚の代表とよく聞きます", password: "123123"}
+    {email: "m@m", name: "鮪", profile: "魚の代表とよく聞きます。", password: "123123"}
+  ]
+)
+
+Attempt.create!(
+  [
+    {user_id:1, content: "テストを頑張る！", is_published_flag:true, part:0} 
+  ]
+)
+
+Tag.create!(
+ [
+   {tag_type: "test1"},
+   {tag_type: "test2"},
+   {tag_type: "test3"},
+   {tag_type: "test4"},
+   {tag_type: "test5"},
+   {tag_type: "test6"}
+  ]
+)
+
+AttemptAndTag.create!(
+  [
+    {attempt_id: 1, tag_id: 1}
   ]
 )
